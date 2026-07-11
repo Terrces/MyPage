1dc479e2fd1b7509d3737bf7cae75bf52cb69d42
tercs <113709421+Terrces@users.noreply.github.com>
2026-07-12 02:45:26 +0500

    cleaning code and small change

--- modules/utils/links.js
+++ modules/utils/links.js
@@ -4,13 +4,15 @@ const jsonObject = await Fetchjson.json()
 const container = document.querySelector(".LinksContainer")
 
 for (const link in jsonObject){
-    if(jsonObject.hasOwnProperty(link)){
-        createButtons(jsonObject[link])
-    }
+    if(jsonObject.hasOwnProperty(link)){ createButtons(jsonObject[link]) }
 }
 
 function createButtons(element){
     let button = document.createElement("button")
+    const mouseEnteredPosition = "0.3em"
+    const backgroundSizeDefault = "1.8em"
+    const backgroundSizeHovered = "1.5em"
+    const hoveredRadius = "0.5em"
 
     button.style.margin = "0.2em"
     button.style.cursor = "pointer"
@@ -30,28 +32,24 @@ function createButtons(element){
 
     button.style.minWidth = "2em"
     button.style.minHeight = "2.4em"
-
-    button.addEventListener("mouseenter", () => {
-        button.style.backgroundPositionY = "0.3em"
-        button.style.backgroundPositionX = "0.3em"
-        button.style.backgroundSize = "1.5em"
-        button.style.borderRadius = "0.5em"
-    })
-
+    
     button.addEventListener("mouseout", () => {
         button.style.backgroundPositionY = "0em"
         button.style.backgroundPositionX = "0em"
-        button.style.backgroundSize = "1.8em"
+        button.style.backgroundSize = backgroundSizeDefault
         button.style.borderRadius = "0em"
     })
 
-    button.addEventListener("click",() => {
-        openLink(element.url)
+    button.addEventListener("mouseenter", () => {
+        button.style.backgroundPositionY = mouseEnteredPosition
+        button.style.backgroundPositionX = mouseEnteredPosition
+        button.style.backgroundSize = backgroundSizeHovered
+        button.style.borderRadius = hoveredRadius
     })
+
+    button.addEventListener("click",() => {
+        window.open(element.url,'_blank').focus();
+    })
+    
     container.appendChild(button)
-}
-
-
-function openLink(url){
-    window.open(url,'_blank').focus();
 }
